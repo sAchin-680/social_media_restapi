341130738002396d0fb73d5703f3e14209e1409c
Sachin <mrmister680@gmail.com>
2024-11-22 11:11:21 +0530

    Refactor authentication routes; enhance user registration with fullName and bio fields, improve error handling, and fix minor bugs

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 const express = require('express');
 require('express-async-errors');
-
-const connectDB = require('./config/db');
 const dotenv = require('dotenv');
 const cookieParser = require('cookie-parser');
-const autthRoute = require('./routes/auth');
+const connectDB = require('./config/db');
+const authRoute = require('./routes/auth');
 
 dotenv.config();
 
@@ -12,11 +11,12 @@ const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
-app.use('/api/auth', autthRoute);
 
 connectDB();
 
-app.use(express.json());
-app.listen(5000, () => {
-  console.log(`Server is running on port ${5000}`);
+app.use('/api/auth', authRoute);
+
+const PORT = 5000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
--- routes/auth.js
+++ routes/auth.js
@@ -8,89 +8,102 @@ const router = express.Router();
 // Register
 router.post('/register', async (req, res) => {
   try {
-    const { username, email, password } = req.body;
-    const existingUser = await User.findOne({ $or: [{ username }, { email }] });
+    const { userName, email, password, fullName, bio } = req.body;
+
+    if (!userName || !email || !password) {
+      return res.status(400).json('All fields are required');
+    }
+
+    const existingUser = await User.findOne({
+      $or: [{ userName }, { email }, { fullName }, { bio }],
+    });
 
     if (existingUser) {
       return res.status(400).json('User already exists');
     }
 
     const salt = await bcrypt.genSalt(10);
-    const hashedPassword = await bcrypt.hashSync(password, salt);
+    const hashedPassword = await bcrypt.hash(password, salt);
 
-    const newUser = new User({ ...req.body, password: hashedPassword });
+    const newUser = new User({
+      userName,
+      email,
+      fullName,
+      bio,
+      password: hashedPassword,
+    });
     const savedUser = await newUser.save();
 
     return res.status(201).json(savedUser);
+  } catch (error) {
+    if (error.name === 'ValidationError') {
+      return res.status(400).json({ message: error.message });
+    }
+
+    res.status(500).json({ message: 'Server error' });
+  }
+});
+
+// Login
+router.post('/login', async (req, res) => {
+  try {
+    const user = req.body.email
+      ? await User.findOne({ email: req.body.email })
+      : await User.findOne({ username: req.body.username });
+
+    if (!user) {
+      return res.status(404).json('User not found');
+    }
+
+    const match = await bcrypt.compare(req.body.password, user.password);
+    if (!match) {
+      return res.status(401).json('Wrong Credentials');
+    }
+
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+      expiresIn: '24h',
+    });
+    // console.log(token);
+
+    const { password, ...data } = user.toObject();
+
+    res.cookie('token', token, { httpOnly: true }).status(200).json(data);
   } catch (error) {
     console.error(error.message);
     res.status(500).json('Server error');
   }
 });
 
-// Login
-// router.post('/login', async (req, res) => {
+// Logout
+// router.get('/logout', async (req, res) => {
 //   try {
-//     let user;
-
-//     if (req.body.email) {
-//       user = await User.findOne({ email: req.body.email });
-//     } else {
-//       user = await User.findOne({ username: req.body.username });
-//     }
-
-//     if (!user) {
-//       res.status(404).json('User not found');
-//     }
-
-//     const match = await bcrypt.compare(req.body.password, user.password);
-
-//     if (!match) {
-//       res.status(401).json('Wrong Credentials');
-//       const { password, ...data } = user.doc;
-//       const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
-//         expiresIn: '24h',
-//         res.cookie('token', token).status(200).json(data),
-//       });
-
-//       res.status(200).json(user);
-//     }
+//     res
+//       .clearCookie('token', { sameSite: 'none', secure: true })
+//       .status(200)
+//       .json('Logged out');
 //   } catch (error) {
 //     console.error(error.message);
 //     res.status(500).json('Server error');
 //   }
 // });
 
-// Logout
-router.get('/logout', async (req, res) => {
-  try {
-    res
-      .clearCookie('token', { sameSite: 'none', secure: true })
-      .status(200)
-      .json('Logged out');
-  } catch (error) {
-    console.error(error.message);
-    res.status(500).json('Server error');
-  }
-});
-
 // Get current user
-router.get('/refetch', async (req, res) => {
-  const token = req.cookies.token;
-  jwt.verify(token, process.env.JWT_SECRET, async (err, data) => {
-    if (err) {
-      return res.status(404).json('Unauthorized');
-    }
-    try {
-      const id = data.id;
-      const user = await User.findByOne({ _id: id });
-      res.status(200).json(user);
-    } catch (error) {
-      console.error(error.message);
-      res.status(500).json('Server error');
-    }
-  });
-});
+// router.get('/refetch', async (req, res) => {
+//   const token = req.cookies.token;
+//   jwt.verify(token, process.env.JWT_SECRET, async (err, data) => {
+//     if (err) {
+//       return res.status(404).json('Unauthorized');
+//     }
+//     try {
+//       const id = data.id;
+//       const user = await User.findByOne({ _id: id });
+//       res.status(200).json(user);
+//     } catch (error) {
+//       console.error(error.message);
+//       res.status(500).json('Server error');
+//     }
+//   });
+// });
 // Update user
 
 // Delete user
