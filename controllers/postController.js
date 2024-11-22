0ef6a087d7e596a10454c09a8adff16591ec361b
Sachin <mrmister680@gmail.com>
2024-11-22 17:25:53 +0530

    Add functionality to create posts with images; implement createPostWithImagesController and corresponding route; enhance post creation capabilities

--- controllers/postController.js
+++ controllers/postController.js
@@ -29,4 +29,40 @@ const createPostController = async (req, res, next) => {
   }
 };
 
-module.exports = { createPostController };
+const generateFileUrl = (filename) => {
+  return process.env.URL + `/uploads/${filename}`;
+};
+
+const createPostWithImagesController = async (req, res, next) => {
+  const { userID } = req.params;
+  const { caption } = req.body;
+
+  try {
+    const user = await User.findById(userID);
+    if (!user) {
+      throw new CustomError('User not found', 404);
+    }
+
+    const imageUrl = req.files.map((file) => generateFileUrl(file.filename));
+
+    const newPost = new Post({
+      user: userID,
+      caption,
+      images: imageUrl,
+    });
+
+    await newPost.save();
+
+    user.posts.push(newPost._id);
+    await user.save();
+
+    res.status(201).json({
+      message: 'Post created successfully',
+      post: newPost,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports = { createPostController, createPostWithImagesController };
--- routes/posts.js
+++ routes/posts.js
@@ -1,9 +1,19 @@
 const express = require('express');
 const router = express.Router();
 const upload = require('../middlewares/upload');
-const { createPostController } = require('../controllers/postController');
+const {
+  createPostController,
+  createPostWithImagesController,
+} = require('../controllers/postController');
 
 // CREATE POST
 router.post('/create', createPostController);
+
+// CREATE POST WITH IMAGES
+router.post(
+  '/create-with-images',
+  upload.array('images', 5),
+  createPostWithImagesController
+);
 
 module.exports = router;
