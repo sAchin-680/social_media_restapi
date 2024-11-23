05e1b8fa1b071aaffdc08fcd066f97d6b28865aa
Sachin <mrmister680@gmail.com>
2024-11-23 15:46:42 +0530

    Add conversation and message functionality; implement conversationController and messageController; enhance messaging capabilities

--- index.js
+++ index.js
@@ -8,6 +8,9 @@ const userRoute = require('./routes/users');
 const path = require('path');
 const postRoute = require('./routes/posts');
 const commentRoute = require('./routes/comments');
+const storiesRoute = require('./routes/stories');
+const Conversation = require('./models/Conversation');
+const Message = require('./models/Message');
 
 // Middlwares
 const { errorHandler } = require('./middlewares/error');
@@ -28,6 +31,9 @@ app.use('/api/users', userRoute);
 app.use('/api/auth', authRoute);
 app.use('/api/posts', postRoute);
 app.use('/api/comments', commentRoute);
+app.use('/api/stories', storiesRoute);
+app.use('/api/conversations', verifyToken, conversationRoute);
+app.use('/api/messages', verifyToken, messageRoute);
 
 const PORT = 5000;
 app.listen(PORT, () => {
