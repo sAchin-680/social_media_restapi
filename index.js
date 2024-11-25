bd035d3ea3b71b8e553d3adca0798c1eb39abf23
Sachin <mrmister680@gmail.com>
2024-11-25 21:21:04 +0530

    Refactor database connection; move connectDB to database directory, update import paths, and standardize API route naming conventions.

--- index.js
+++ index.js
@@ -1,41 +1,35 @@
 const express = require('express');
-require('express-async-errors');
+const connectDB = require('./database/db');
+const app = express();
 const dotenv = require('dotenv');
 const cookieParser = require('cookie-parser');
-const connectDB = require('./config/db');
 const authRoute = require('./routes/auth');
 const userRoute = require('./routes/users');
-const path = require('path');
 const postRoute = require('./routes/posts');
 const commentRoute = require('./routes/comments');
-const storiesRoute = require('./routes/stories');
-const Conversation = require('./models/Conversation');
-const Message = require('./models/Message');
-
-// Middlwares
+const storyRoute = require('./routes/stories');
+const conversationRoute = require('./routes/conversations');
+const messageRoute = require('./routes/messages');
+const path = require('path');
 const { errorHandler } = require('./middlewares/error');
+const verifyToken = require('./middlewares/verifyToken');
 
 dotenv.config();
-
-const app = express();
-
 app.use(express.json());
 app.use(cookieParser());
-
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+
+app.use('/api/auth', authRoute);
+app.use('/api/user', verifyToken, userRoute);
+app.use('/api/post', verifyToken, postRoute);
+app.use('/api/comment', verifyToken, commentRoute);
+app.use('/api/story', verifyToken, storyRoute);
+app.use('/api/conversation', verifyToken, conversationRoute);
+app.use('/api/message', verifyToken, messageRoute);
+
 app.use(errorHandler);
 
-connectDB();
-
-app.use('/api/users', userRoute);
-app.use('/api/auth', authRoute);
-app.use('/api/posts', postRoute);
-app.use('/api/comments', commentRoute);
-app.use('/api/stories', storiesRoute);
-app.use('/api/conversations', verifyToken, conversationRoute);
-app.use('/api/messages', verifyToken, messageRoute);
-
-const PORT = 5000;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+app.listen(5000, () => {
+  connectDB();
+  console.log('app is running');
 });
