3576b9f6fa69055ee91452de9580766e11625e48
Sachin <mrmister680@gmail.com>
2024-12-05 02:49:14 +0530

    Fix import path for Post model in postController.js

--- controllers/postController.js
+++ controllers/postController.js
@@ -1,4 +1,4 @@
-const Post = require('../models/Post');
+const Post = require('../models/Posts');
 const User = require('../models/User');
 const { CustomError } = require('../middlewares/error');
 
